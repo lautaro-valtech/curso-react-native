20b459c7c99a7aeb4745faf052f4766f36a0327c
lautaronahuelc <lautaro.coria@valtech.com>
2023-06-14 12:46:17 -0300

    typescript errors doesn't appear (update)

--- 02-MiPrimeraApp/App.tsx
+++ 02-MiPrimeraApp/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { HolaMundoScreen } from './src/screens/HolaMundoScreen';
+// import { HolaMundoScreen } from './src/screens/HolaMundoScreen';
 import { CounterScreen } from './src/screens/CounterScreen';
 
 const App = () => {
